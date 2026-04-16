59f955c966b7f76eae1ae8b9b5c1728db2fe7d5c
thankiprem73 <thanki.prem73@gmail.com>
2026-04-16 21:16:18 +1000

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const multer = require("multer");
 const nodemailer = require("nodemailer");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+}));
 app.use(express.json());
 
 // 📁 File storage
